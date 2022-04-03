784a74899f1cefbd632094a0e6d5b81b72aa487d
= <=>
2022-04-03 18:39:47 -0300

    feat: calc com dependentes

---
+++ calculation.php
@@ -0,0 +1,43 @@
+<?php
+function calc_irrf(float $sal_base_ir)
+{
+	$faixa1 = 1903.98;
+	$faixa2 = 2826.65;
+	$faixa3 = 3751.05;
+	$faixa4 = 4664.68;
+	if ($sal_base_ir <= $faixa1) {
+		return 0;
+	} else if ($sal_base_ir <= $faixa2) {
+		return ($sal_base_ir * 0.75) - 142.8;
+	} else if ($sal_base_ir <= $faixa3) {
+		return ($sal_base_ir * 0.15) - 354.80;
+	} else if ($sal_base_ir <= $faixa4) {
+		return ($sal_base_ir * 0.225) - 636.13;
+	} else if ($sal_base_ir > $faixa4) {
+		return ($sal_base_ir * 0.275) - 869.36;
+	}
+}
+
+function calc_inss(float $sal_bruto)
+{
+	$faixa1 = 1212.00;
+	$faixa2 = 2427.35;
+	$faixa3 = 3641.03;
+	$faixa4 = 7087.22;
+	$teto =  828.39;
+
+	return $teto;
+	if ($sal_bruto > $faixa4) {
+		return $teto;
+	} else {
+		if ($sal_bruto <= $faixa1) {
+			return $sal_bruto * 0.075;
+		} else if ($sal_bruto <= $faixa2) {
+			return ($sal_bruto * 0.09) - 18.18;
+		} else if ($sal_bruto <= $faixa3) {
+			return ($sal_bruto * 0.12) - 91;
+		} else if ($sal_bruto <= $faixa4) {
+			return ($sal_bruto * 0.14) - 163.82;
+		}
+	}
+}
--- index.php
+++ index.php
@@ -1,70 +1,30 @@
 <?php
+include 'calculation.php';
 
-if(isset($_POST['submit'])) {
+if (isset($_POST['submit'])) {
   $sal_bruto = htmlspecialchars($_POST['sal_bruto']);
   $deps = htmlspecialchars($_POST['dependentes']);
+  var_dump([$sal_bruto, $deps]);
 
   // In a very stupid way
-  $inss1 = 1212.00;
-  $inss2 = 2427.35;
-  $inss3 = 3641.03;
-  $inss4 = 7087.22;
+  $inss_calc = calc_inss($sal_bruto);
+  $sal_base_ir = $sal_bruto - $inss_calc - (189.59 * $deps);
+  $irrf_calc = calc_irrf($sal_base_ir, $deps);
 
-  $inss_bracket_1 = $inss1 * 0.075;
-  $inss_bracket_2 = $inss2 * 0.09;
-  $inss_bracket_3 = $inss3 * 0.12;
-  $inss_bracket_4 = $inss4 * 0.14;
-  $inss_ceiling = $inss_bracket_4 + $inss_bracket_3 + $inss_bracket_2 + $inss_bracket_1;
-
-  $irrf1 = 1903.98 ;
-  $irrf2 = 2826.65;
-  $irrf3 = 3751.05;
-  $irrf4 = 4664.68;
-
-  $inss_calc = 0;
-  $irrf_calc = 0;
-  $sal_base_ir = 0;
-
-  if ($sal_bruto > $inss4) {
-    $inss_calc = $inss_ceiling;
-  } else {
-    if ($sal_bruto <= $inss1) {
-      $inss_calc = $sal_bruto * 0.075;
-    } else if ($sal_bruto <= $inss2) {
-      $inss_calc = (($sal_bruto - $inss1) * 0.09) + $inss_bracket_1;
-    } else if ($sal_bruto <= $inss3) {
-      $inss_calc = (($sal_bruto - $inss2) * 0.12) + $inss_bracket_1 + $inss_bracket_2;
-    } else if ($sal_bruto <= $inss4) {
-      $inss_calc = (($sal_bruto-$inss3) * 0.14) + $inss_bracket_1 + $inss_bracket_2+ $inss_bracket_3;
-    }
-  }
-
-  $sal_base_ir = $sal_bruto - $inss_calc;
-
-  if ($sal_base_ir <= $irrf1) {
-    $irrf_calc = 0;
-  } else if ($sal_base_ir <= $irrf2) {
-    $irrf_calc = ($sal_base_ir * 0.75) - 142.8;
-  } else if ($sal_base_ir <= $irrf3) {
-    $irrf_calc = ($sal_base_ir * 0.15) - 354.80;
-  } else if ($sal_base_ir <= $irrf4) {
-    $irrf_calc = ($sal_base_ir * 0.225) - 636.13;
-  } else if ($sal_base_ir > $irrf4) {
-    $irrf_calc = ($sal_base_ir * 0.275) - 869.36;
-  }
-
-  echo "INSS" . $inss_calc;
-  echo "IRRF" . $irrf_calc;
+  $perc_efetiva_inss = $inss_calc / $sal_bruto * 100;
+  $perc_efetiva_irrf = $irrf_calc / $sal_bruto * 100;
 }
 
 ?>
 
 <!DOCTYPE html>
 <html lang="en">
+
 <head>
   <meta charset="UTF-8">
   <title>Salariator - Calcule diferença entre CLT e CNPJ</title>
 </head>
+
 <body>
   <h1>Software em ALPHA. Não está pronto para uso</h1>
 
@@ -124,18 +84,23 @@ if(isset($_POST['submit'])) {
       <td>14%</td>
     </tr>
   </table>
-  <form action="<?php echo $_SERVER['PHP_SELF']?>" method="post">
-    <label for="sal_bruto"/>Salário bruto mensal</label>
-    <input type="text" name="sal_bruto" />
+  <form action="<?php echo $_SERVER['PHP_SELF'] ?>" method="post">
+    <label for="sal_bruto">Salário bruto mensal</label>
+    <input type="number" name="sal_bruto" />
 
-    <label for="dependentes"/>Num. Dependentes</label>
-    <input type="text" name="dependentes" />
+    <label for="dependentes">Num. Dependentes</label>
+    <input type="number" name="dependentes" />
 
     <input type="submit" name="submit" value="Calcular">
   </form>
   <p>Salário Bruto Mensal: <?= $sal_bruto ?></p>
   <p>Base IR: <?= $sal_base_ir ?></p>
+
   <p>INSS Calculado: <?= $inss_calc ?></p>
   <p>IRRF Calculado: <?= $irrf_calc ?></p>
+
+  <p>Aliquota efetiva do INSS: <?= $perc_efetiva_inss ?> %</p>
+  <p>Aliquota efetiva do IRRF: <?= $perc_efetiva_irrf ?> %</p>
 </body>
+
 </html>
