94ae369605af0aef0593f643a85f30384a37ac7a
Lucas Ramos <lucasnramos@gmail.com>
2022-06-05 12:27:50 -0300

    feat: add simple bootstrap and core classes

---
+++ src/public/index.php
@@ -0,0 +1,14 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+	<meta charset="UTF-8">
+	<title></title>
+</head>
+<body>
+<?php 
+require_once '../app/autoloader.php';
+$app = new App;
+?>
+	<h1>index from public</h1>
+</body>
+</html>
