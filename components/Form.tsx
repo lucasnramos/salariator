1c18106b221f20f4a6411d2a32db96380998e945
Lucas Ramos <lucasnramos@gmail.com>
2022-12-11 13:29:40 -0300

    feat: add required field and marker

--- components/Form.tsx
+++ components/Form.tsx
@@ -2,65 +2,77 @@ import React, { FormEvent } from "react";
 import FormInput, { FormInputProps } from "./FormInput";
 
 function Form() {
-  const cltFormInputs: FormInputProps[] = [
-    {
-      name: "salarioBrutoMensal",
-      label: "Salario bruto",
-    },
-    {
-      name: "numDependentes",
-      label: "Num. Dependentes",
-    },
-    {
-      name: "totalBeneficios",
-      label: "Outros Benefícios",
-    },
-    {
-      name: "totalDescontos",
-      label: "Outros Descontos",
-    },
-  ];
+	const cltFormInputs: FormInputProps[] = [
+		{
+			name: "salarioBrutoMensal",
+			label: "Salario bruto",
+			required: true
+		},
+		{
+			name: "numDependentes",
+			label: "Num. Dependentes",
+			required: true
+		},
+		{
+			name: "totalBeneficios",
+			label: "Outros Benefícios",
+			required: true
+		},
+		{
+			name: "totalDescontos",
+			label: "Outros Descontos",
+			required: true
+		},
+	];
 
-  const cnpjFormInputs: FormInputProps[] = [
-    {
-      name: "faturamentoMensal",
-      label: "Faturamento do último mês",
-    },
-    {
-      name: "rtb12",
-      label: "Receita Bruta dos últimos 12 meses (RTB12)",
-    },
-    {
-      name: "usarFatorR",
-      label: "Utilizar Fator R",
-      type: "checkbox",
-    },
-    {
-      name: "proLabore",
-      label: "Pro-labore",
-    },
-  ];
+	const cnpjFormInputs: FormInputProps[] = [
+		{
+			name: "faturamentoMensal",
+			label: "Faturamento do último mês",
+			required: true
+		},
+		{
+			name: "rtb12",
+			label: "Receita Bruta dos últimos 12 meses (RTB12)",
+			required: true
+		},
+		{
+			name: "usarFatorR",
+			label: "Utilizar Fator R",
+			type: "checkbox",
+		},
+		{
+			name: "proLabore",
+			label: "Pro-labore",
+			required: true
+		},
+	];
 
-  return (
-    <form className="my-5" action="/api/form" method="post">
-      <h2 className="mt-6 mb-4">Dados CLT</h2>
-      {cltFormInputs.map((input, index) => (
-        <FormInput key={index} {...input} />
-      ))}
+	return (
+		<form className="my-5" action="/api/form" method="post">
+			<div className="flex">
+				<div className='mr-4 flex-grow'>
+					<h2 className="mt-6 mb-4">Dados CLT</h2>
+					{cltFormInputs.map((input, index) => (
+						<FormInput key={index} {...input} />
+					))}
+				</div>
+				<div className='ml-4'>
+					<h2 className="mt-6 mb-4">Dados PJ</h2>
+					{cnpjFormInputs.map((input, index) => (
+						<FormInput key={index} {...input} />
+					))}
+				</div>
+			</div>
 
-      <h2 className="mt-6 mb-4">Dados PJ</h2>
-      {cnpjFormInputs.map((input, index) => (
-        <FormInput key={index} {...input} />
-      ))}
-
-      <input
-        type="submit"
-        name="submit"
-        value="Calcular"
-        className="block px-4 py-2 font-semibold text-medium bg-sky-500 text-white rounded-full shadow-sm"
-      />
-    </form>
-  );
+			<input
+				type="submit"
+				name="submit"
+				value="Calcular"
+				className="text-center px-4 py-2 font-semibold text-medium bg-sky-500 text-white rounded-full shadow-sm"
+			/>
+		</form>
+	);
 }
 
 export default Form;
