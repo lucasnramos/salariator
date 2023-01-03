4b395cf505fe8ba1a738dc46a8e16b63e64173b6
Lucas Ramos <lucas.ramos@gft.com>
2023-01-03 13:41:04 -0300

    feat: change "services" dir to "server"

--- pages/api/form.ts
+++ pages/api/form.ts
@@ -1,6 +1,6 @@
 // Next.js API route support: https://nextjs.org/docs/api-routes/introduction
 import type { NextApiRequest, NextApiResponse } from "next";
-import { clt, cnpj } from "../../services/calculation";
+import { clt, cnpj } from "../../server/calculation";
 
 export default function handler(req: NextApiRequest, res: NextApiResponse) {
   if ((req.method = "GET")) {
