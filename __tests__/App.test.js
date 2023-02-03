ae1fd02dc38ae0ca191f11409025578d3b401293
Lucas Ramos <lucasnramos@gmail.com>
2023-02-03 17:46:11 -0300

    feat: add simple test for FormInput

--- __tests__/App.test.js
+++ __tests__/App.test.js
@@ -1,13 +1,17 @@
 import { render, screen } from "@testing-library/react"
 import Home from "../pages/index"
+import FormInput from "../components/FormInput"
+import '@testing-library/jest-dom'
 
-test('Dummy test', () => {
-  expect(2).toBe(2)
-})
-
-test('Should show title', () => {
+test('should show title', () => {
   render(<Home />)
 
   expect(screen.getByText('Salariator - Compare salários CLT e PJ')).toBeTruthy()
+})
+
+// Form Inputs
+test('should show an asterix if input is required', () => {
+  render(<FormInput label="this is my label" required />)
+  expect(screen.getByText('*')).toBeVisible()
 })
 
