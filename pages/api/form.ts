17e0e9af7a0fe072db9f4e24d59a9a8d7fec2610
Lucas Ramos <lucasnramos@gmail.com>
2024-02-02 16:19:25 -0300

    add initial yearly calculation for CLT and CNPJ

--- pages/api/form.ts
+++ pages/api/form.ts
@@ -1,6 +1,6 @@
 // Next.js API route support: https://nextjs.org/docs/api-routes/introduction
 import type { NextApiRequest, NextApiResponse } from "next";
-import { clt, cnpj } from "../../server/calculation";
+import { clt, cnpj, cltAnual, cnpjAnual } from "../../server/calculation";
 
 export default function handler(req: NextApiRequest, res: NextApiResponse) {
   if ((req.method = "GET")) {
@@ -9,10 +9,14 @@ export default function handler(req: NextApiRequest, res: NextApiResponse) {
 
   const cltCalculado = clt(req.body);
   const cnpjCalculado = cnpj(req.body);
+  const cltAnualCalculado = cltAnual(cltCalculado);
+  const cnpjAnualCalculado = cnpjAnual(cnpjCalculado);
 
   const response = {
     cltCalculado,
     cnpjCalculado,
+    cltAnualCalculado,
+    cnpjAnualCalculado,
   };
 
   res.status(200).json(response);
